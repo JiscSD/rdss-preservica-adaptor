79fb0327d495613da16eacfa4598898254c8358e
Mark Winterbottom <mark@londonappdeveloper.com>
2017-08-25 14:40:53 +0100

    Set the `aws_route_table.private` back to what it was before.

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -92,11 +92,6 @@ resource "aws_subnet" "private" {
 resource "aws_route_table" "private" {
   vpc_id = "${aws_vpc.vpc.id}"
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.igw.id}"
-  }
-
   tags {
     Name        = "${var.project}-${terraform.env}-private"
     Environment = "${terraform.env}"
