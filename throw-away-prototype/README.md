98a2b5558ce8f2d475e3a6e04d6d6ed5b59cb7a6
Michal Charemza <michal@charemza.name>
2018-02-28 16:16:04 +0000

    [RDSS-1900] Fetch API access token

--- throw-away-prototype/README.md
+++ throw-away-prototype/README.md
@@ -12,5 +12,9 @@ The aim of this is to ensure the following is supported by the new Preservica AP
 docker build -t preservica-prototype . && \
 docker run \
   --rm \
+  --env BASE_URL=https://beta.preservica.com/ \
+  --env TENANT=demo \
+  --env USERNAME=alan.mackenzie@digirati.com \
+  --env PASSWORD=_password_ \
   preservica-prototype python run.py
 ```
--- throw-away-prototype/run.py
+++ throw-away-prototype/run.py
@@ -1,1 +1,31 @@
-print("Skeleton throw-away script")
+import logging
+import os
+import requests
+import sys
+
+
+stream_handler = logging.StreamHandler(sys.stdout)
+stream_handler.setLevel(logging.DEBUG)
+stream_handler.setFormatter(
+    logging.Formatter(
+        '%(name)s - %(levelname)s - %(message)s',
+    ),
+)
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+logger.addHandler(stream_handler)
+
+base_url = os.environ['BASE_URL']
+tenant = os.environ['TENANT']
+username = os.environ['USERNAME']
+password = os.environ['PASSWORD']
+
+logging.info('Fetching token...')
+token_response = requests.post(
+    base_url + 'api/accesstoken/login', params={
+        'tenant': tenant,
+        'username': username,
+        'password': password,
+    },
+)
+logging.info(token_response.json())
