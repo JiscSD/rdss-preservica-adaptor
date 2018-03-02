7b6ed36cc3a6ceb282558a44525edc288af962bf
Michal Charemza <michal@charemza.name>
2018-03-02 20:42:50 +0000

    [RDSS-1928] Test to ensure if unable to download, a message sent to error stream

--- tests/test_processor.py
+++ tests/test_processor.py
@@ -76,3 +76,34 @@ def test_record_with_invalid_rdss_message_sends_message_to_error_stream():
     message = json.loads(records[0]['Data'].decode('utf-8'))
     assert message['messageBody']['code'] == 'GENERR004'
     assert 'Invalid, missing or corrupt headers' in message['messageBody']['message']
+
+
+@moto.mock_kinesis
+def test_record_unable_to_download_sends_messages_to_error_stream():
+    client = boto3.client('kinesis', 'eu-west-1')
+    client.create_stream(StreamName='error-stream', ShardCount=1)
+    config = Config(
+        input_stream_name='input-stream',
+        input_stream_region='eu-west-1',
+        error_stream_name='error-stream',
+        error_stream_region='eu-west-1',
+        organisation_buckets={
+            44: 's3://some-bucket/',
+        },
+    )
+    processor = RecordProcessor(config=config)
+
+    with open('tests/fixtures/create.json', 'rb') as fixture_file:
+        fixture = fixture_file.read()
+
+    class FakeRecord():
+        data = base64.b64encode(fixture)
+
+    processor.process_records([FakeRecord()], None)
+
+    records = _get_records(client, 'error-stream')
+    assert len(records) == 1
+
+    message = json.loads(records[0]['Data'].decode('utf-8'))
+    assert message['messageBody']['code'] == 'GENERR009'
+    assert 'Connection refused' in message['messageBody']['details']
