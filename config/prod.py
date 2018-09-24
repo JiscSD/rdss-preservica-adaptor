0f6acd4249207c22b77d158790744e560abe5a5d
Finlay McCourt <finlay.mccourt@digirati.com>
2018-09-24 13:03:46 +0100

    Now removing middlesex.

--- config/prod.py
+++ config/prod.py
@@ -5,6 +5,4 @@ invalid_stream_name = 'message_invalid_prod'
 error_stream_name = 'message_error_prod'
 adaptor_aws_region = 'eu-west-2'
 
-organisation_buckets = {
-    '89': 's3://preservica-89-api-prod-autoupload',
-}
+organisation_buckets = {}
