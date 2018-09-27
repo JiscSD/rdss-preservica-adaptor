988432655f85b79294fa456403c614bed267625a
Finlay McCourt <finlay.mccourt@digirati.com>
2018-09-27 14:01:46 +0100

    Removing 471 from the config.

--- config/uat.py
+++ config/uat.py
@@ -5,6 +5,4 @@ invalid_stream_name = 'message_invalid_uat'
 error_stream_name = 'message_error_uat'
 adaptor_aws_region = 'eu-west-2'
 
-organisation_buckets = {
-    '471': 's3://uk.ac.jisc.alpha.researchdata.s3.uoj.autoupload',
-}
+organisation_buckets = {}
